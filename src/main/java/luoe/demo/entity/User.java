d666391750e62a97fe8f93fadfa3de11c9c0d84d
Evelyn <luo.e@yahoo.com>
2020-06-30 16:37:59 +0800

    user(record)/get/{name}({id}) pass

---
+++ src/main/java/luoe/demo/entity/User.java
@@ -0,0 +1,48 @@
+package luoe.demo.entity;
+
+/**
+ * @Data 2020/6/30 11:00
+ * @Author ruary
+ * @Version 1.0
+ * @Describe
+ **/
+public class User {
+
+    private String name;
+    private String password;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
